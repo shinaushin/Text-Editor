088a3ccc79c2526d989afdd3a49ab9889610faf4
Austin Shin <shinaushin@gmail.com>
2014-08-03 15:06:46 -0700

    Create collab.java
    initiates server

---
+++ collab.java
@@ -0,0 +1,14 @@
+/*
+    @author Tu Nguyen, Austin Shin, Dorian Chan
+*/
+public class Collab {    
+	public static void main (String[] args)
+	{
+		try {
+			Server server = new Server();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
